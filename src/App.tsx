33f4103f32d38990ca3ff3519a29ffa217ca73d7
Jefcode <am.amir.husein@gmail.com>
2023-09-07 17:09:36 +0330

    Create general Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,12 @@
-import { useAnimate, stagger } from 'framer-motion';
-import { useEffect } from 'react';
 import Dashboard from './components/Dashboard';
-import Sidebar from './components/sidebar/Sidebar';
-import useSidebar from './hooks/useSidebar';
-import classNames from './utils/classNames';
+import Layout from './components/layout/Layout';
 
 function App() {
-	const [scope, animate] = useAnimate();
-	const { isOpen: isSidebarOpen } = useSidebar();
-
-	// Remove the loader once the app mounts
-	useEffect(() => {
-		removeLoader();
-
-		// Animate Each item that has 'card' class, one by one
-		animate(
-			'.card',
-			{ opacity: 1 },
-			{ delay: stagger(0.5, { startDelay: 1 }) }
-		);
-	}, [animate]);
-
-	return (
-		<>
-			{/* Wrapper */}
-			<div className="fixed top-0 left-0 bottom-0 right-0 flex overflow-hidden">
-				<Sidebar />
-
-				{/* Main Content */}
-				<div className="flex-1 bg-stone-100" ref={scope}>
-					<main
-						className={classNames(
-							'py-8 h-full transition-all duration-300  overflow-y-auto px-2 sm:px-8 max-w-[100vw]',
-							!isSidebarOpen ? 'lg:px-24' : ''
-						)}
-					>
-						<Dashboard />
-					</main>
-				</div>
-			</div>
-		</>
-	);
-}
-
-// Remove preloader on component mount
-function removeLoader() {
-	const loader = document.querySelector('.loader-wrapper') as HTMLDivElement;
-	loader.classList.add('opacity-0', 'invisible');
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,60 @@
+import { stagger, useAnimate } from 'framer-motion';
+import { useEffect, useCallback } from 'react';
+
+import useSidebar from '../../hooks/useSidebar';
+import classNames from '../../utils/classNames';
+import Sidebar from '../sidebar/Sidebar';
+
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+  const [scope, animate] = useAnimate();
+  const { isOpen: isSidebarOpen } = useSidebar();
+
+  const animateCards = useCallback(async () => {
+    // Animate Each item that has 'card' class, one by one
+    await animate(
+      '.card',
+      { opacity: 1 },
+      { delay: stagger(0.5, { startDelay: 1 }) }
+    );
+  }, [animate]);
+
+  // Remove the loader once the app mounts
+  useEffect(() => {
+    removeLoader();
+
+    animateCards();
+  }, [animate, animateCards]);
+
+  return (
+    <>
+      {/* Wrapper */}
+      <div className='fixed top-0 left-0 bottom-0 right-0 flex overflow-hidden'>
+        <Sidebar />
+
+        {/* Main Content */}
+        <div className='flex-1 bg-stone-100' ref={scope}>
+          <main
+            className={classNames(
+              'py-8 h-full transition-all duration-300  overflow-y-auto px-2 sm:px-8 max-w-[100vw]',
+              !isSidebarOpen ? 'lg:px-24' : ''
+            )}
+          >
+            {children}
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
+
+// Remove preloader on component mount
+function removeLoader() {
+  const loader = document.querySelector('.loader-wrapper') as HTMLDivElement;
+  loader.classList.add('opacity-0', 'invisible');
+}
+
+export default Layout;
