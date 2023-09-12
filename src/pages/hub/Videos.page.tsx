fdbb86ffdc645a555e5f193a6f6aac404a271b95
Jefcode <am.amir.husein@gmail.com>
2023-09-12 14:40:37 +0330

    Add Modal for videos

--- src/pages/hub/Videos.page.tsx
+++ src/pages/hub/Videos.page.tsx
@@ -3,6 +3,7 @@ import Row from '../../components/common/Row';
 import Video from './components/Video';
 
 import { ReactComponent as VideoIcon } from '../../icons/video-active.svg';
+import videos from '../../data/videos';
 
 const VideosPage = () => {
   return (
@@ -19,55 +20,11 @@ const VideosPage = () => {
       {/* Videos Flex Container */}
       <div className='w-full h-[660px] overflow-x-hidden overflow-y-auto pr-3 scrollbar-thin scrollbar-track-slate-200 scrollbar-thumb-primary'>
         <Row>
-          <div className='w-full md:w-1/2 xl:w-1/3 p-3'>
-            <Video
-              image='/imgs/vid-thumb-1.jpg'
-              title='Breathing technique for Anxiety'
-              author='Dr. Shanice McDonald'
-            />
-          </div>
-
-          <div className='w-full md:w-1/2 xl:w-1/3 p-3'>
-            <Video
-              image='/imgs/vid-thumb-2.jpg'
-              title='10 Ways to Manage Stress'
-              live
-              date='May 2, 2023'
-            />
-          </div>
-
-          <div className='w-full md:w-1/2 xl:w-1/3 p-3'>
-            <Video
-              image='/imgs/vid-thumb-3.jpg'
-              title='10 Ways to Manage Stress'
-              author='Dr. Shanice McDonald'
-            />
-          </div>
-
-          <div className='w-full md:w-1/2 xl:w-1/3 p-3'>
-            <Video
-              image='/imgs/vid-thumb-2.jpg'
-              title='10 Ways to Manage Stress'
-              live
-              date='May 2, 2023'
-            />
-          </div>
-
-          <div className='w-full md:w-1/2 xl:w-1/3 p-3'>
-            <Video
-              image='/imgs/vid-thumb-3.jpg'
-              title='10 Ways to Manage Stress'
-              author='Dr. Shanice McDonald'
-            />
-          </div>
-
-          <div className='w-full md:w-1/2 xl:w-1/3 p-3'>
-            <Video
-              image='/imgs/vid-thumb-1.jpg'
-              title='Breathing technique for Anxiety'
-              author='Dr. Shanice McDonald'
-            />
-          </div>
+          {videos.map((video, idx) => (
+            <div key={idx} className='w-full md:w-1/2 xl:w-1/3 p-3'>
+              <Video data={video} />
+            </div>
+          ))}
         </Row>
       </div>
     </div>
--- src/pages/hub/components/Video.tsx
+++ src/pages/hub/components/Video.tsx
@@ -1,33 +1,118 @@
+import * as Slider from '@radix-ui/react-slider';
+
 import { ReactComponent as HeartIcon } from '../../../icons/heart.svg';
 import { ReactComponent as PlayIcon } from '../../../icons/play.svg';
 import { ReactComponent as EditIcon } from '../../../icons/edit.svg';
+import { ReactComponent as PreviousIcon } from '../../../icons/previous.svg';
+import { ReactComponent as NextIcon } from '../../../icons/next.svg';
+import { ReactComponent as PauseIcon } from '../../../icons/pause.svg';
+import { Media } from '../../../types';
+import Modal from '../../../components/common/Modal';
 
 interface VideoProps {
-  image: string;
-  title: string;
-  live?: boolean;
-  date?: string;
-  author?: string;
+  data: Media;
 }
 
-const Video = ({ image, title, live, date, author }: VideoProps) => {
+const Video = ({ data }: VideoProps) => {
   return (
     <div className='w-full'>
       {/* Video Thumbnail Container */}
       <div className='relative rounded-xl overflow-hidden'>
         <img
-          src={image}
+          src={data.image}
           alt='Video thumbnail'
-          className='w-full h-full object-cover'
+          className='w-full h-56 object-cover object-center'
         />
 
         {/* Gradient */}
         <div className='absolute inset-0 bg-gradient-to-t from-stone-900/50 via-transparent to-transparent'></div>
 
         {/* Play Button */}
-        <button className='absolute active:scale-95 transition z-10 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'>
-          <PlayIcon />
-        </button>
+        <Modal>
+          <Modal.Button className='absolute active:scale-95 transition z-10 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'>
+            <PlayIcon />
+          </Modal.Button>
+
+          <Modal.Content
+            dark
+            className='!w-[900px] !h-[550px] !rounded-xl !p-0'
+          >
+            {/* Video Container */}
+            <div className='w-full h-full flex items-center justify-center'>
+              <img
+                src={data.image}
+                alt='Video thumbnail'
+                className='h-full object-cover'
+              />
+
+              {/* Video */}
+              {/* <video controls className='w-full'>
+                <source src='videos/video.mp4' type='video/mp4' />
+                Your browser does not support the video tag.
+              </video> */}
+            </div>
+
+            {/* Controls and progress */}
+            <div className='absolute bottom-8 w-full px-8 before:z-0 before:content-[""] before:fixed before:inset-0 before:bg-gradient-to-t before:from-stone-900/60 before:via-transparent before:to-transparent'>
+              <div className='relative z-20'>
+                {/* Name, Date, Link */}
+                <div className='mb-3'>
+                  <div className='flex items-center gap-2 justify-between'>
+                    <h3 className='flex-1 font-medium text-stone-100'>
+                      {data.title}
+                    </h3>
+
+                    <button className='active:scale-90 transition'>
+                      <HeartIcon className='text-gradientRed-200' />
+                    </button>
+                  </div>
+
+                  <time className='text-stone-300 font-light text-xs'>
+                    10/04/23
+                  </time>
+                </div>
+
+                {/* Video Controls */}
+                <div>
+                  {/* Progress */}
+                  <Slider.Root
+                    className='relative flex items-center select-none touch-none h-5'
+                    defaultValue={[0]}
+                    max={100}
+                    step={1}
+                  >
+                    <Slider.Track className='bg-stone-300 relative grow rounded-full h-[2px]'>
+                      <Slider.Range className='absolute bg-gradient-to-r from-gradientRed-200 to-gradientRed-100 rounded-full h-full' />
+                    </Slider.Track>
+                    <Slider.Thumb
+                      className='block w-4 h-4 bg-gradient-to-r to-gradientRed-200 from-gradientRed-100 rounded-[10px] focus:shadow-blackA8 hover:cursor-pointer'
+                      aria-label='Volume'
+                    />
+                  </Slider.Root>
+
+                  {/* Time Flex Container */}
+                  <div className='flex items-center justify-between mt-3'>
+                    <time className='font-semibold text-stone-200'>1:47</time>
+                    <time className='text-stone-300'>12:21</time>
+                  </div>
+
+                  {/* Pause + Prev and Next buttons */}
+                  <div className='flex items-center justify-center gap-14 text-stone-200'>
+                    <button className='transition active:scale-95'>
+                      <PreviousIcon />
+                    </button>
+                    <button className='transition active:scale-95'>
+                      <PauseIcon />
+                    </button>
+                    <button className='transition active:scale-95'>
+                      <NextIcon />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Modal.Content>
+        </Modal>
 
         {/* Video Info */}
         <div className='absolute z-10 w-full text-white bottom-3 px-4 flex items-center justify-between'>
@@ -39,7 +124,7 @@ const Video = ({ image, title, live, date, author }: VideoProps) => {
 
           {/* duration/Live */}
           <div className='flex items-center gap-1.5'>
-            {live ? (
+            {data.live ? (
               <>
                 <span>Live</span>
                 <div className='w-2 h-2 rounded-full bg-rose-500'></div>
@@ -57,7 +142,9 @@ const Video = ({ image, title, live, date, author }: VideoProps) => {
       {/* Video Title/Author */}
       <div className='mt-3'>
         <div className='flex items-center gap-1 justify-between'>
-          <h3 className='text-xl flex-1 font-medium text-stone-800'>{title}</h3>
+          <h3 className='text-xl flex-1 font-medium text-stone-800'>
+            {data.title}
+          </h3>
 
           <button className='rounded-full w-6 h-6 bg-orange-500 flex items-center justify-center text-sm text-white hover:bg-orange-600 transition active:scale-95'>
             <EditIcon className='w-3 h-3' />
@@ -67,12 +154,12 @@ const Video = ({ image, title, live, date, author }: VideoProps) => {
         {/* Author/Date */}
         <p className='mt-1'>
           {/* Author */}
-          {author && (
+          {data.author && (
             <span className='text-gradientRed-200'>Dr. Shanice McDonald</span>
           )}
 
           {/* Date */}
-          {date && <span className='text-primary '>May 24, 2023</span>}
+          {data.date && <span className='text-primary '>May 24, 2023</span>}
         </p>
       </div>
     </div>
