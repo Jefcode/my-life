8c315bd075a2255c755a7d1c53d78f63f95ed611
Jefcode <am.amir.husein@gmail.com>
2023-10-02 12:58:20 +0330

    Replace the custom toast with the default one for now

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -2,7 +2,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 
 import queryClient from './lib/react-query/queryClient';
-import CustomToast from './components/ui/CustomToast';
+// import CustomToast from './components/ui/CustomToast';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -10,7 +10,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
       {children}
 
       {/* Toast, Customized */}
-      <Toaster>{(t) => <CustomToast toastInstance={t} />}</Toaster>
+      <Toaster />
     </QueryClientProvider>
   );
 };
